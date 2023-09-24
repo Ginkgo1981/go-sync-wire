e7129ab3776f90a1396a8f452365cabf469831ee
Jian <ginkgo985@gmail.com>
2023-09-24 10:41:35 +0200

    init commit

---
+++ internal/config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+type Data struct {
+	Database Database
+}
+
+type Database struct {
+	Driver          string
+	Dsn             string
+	MaxIdleConns    int           `mapstructure:"max_idle_conns"`
+	MaxOpenConns    int           `mapstructure:"max_open_conns"`
+	ConnMaxLifeTime time.Duration `mapstructure:"conn_max_lifetime"`
+}
+
+type App struct {
+	LogSavePath string `mapstructure:"log_save_path"`
+	LogFileName string `mapstructure:"log_file_name"`
+	LogFileExt  string `mapstructure:"log_file_ext"`
+}
+
+type Config struct {
+	vp *viper.Viper
+}
+
+func NewConfig() (*Config, error) {
+	vp := viper.New()
+	vp.SetConfigName("config")
+	vp.AddConfigPath("../../configs/")
+	vp.SetConfigType("yaml")
+	err := vp.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+	return &Config{vp: vp}, nil
+}
+
+func (s *Config) ReadSection(k string, v interface{}) error {
+	err := s.vp.UnmarshalKey(k, v)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
